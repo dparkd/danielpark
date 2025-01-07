b1d2ff3b355bb509a9a5cc7662795b4b49aba9d8
Daniel Park <dpark@Daniels-MacBook-Pro-2.local>
2025-01-07 14:32:41 -0800

    responsive

--- projects/project.js
+++ projects/project.js
@@ -1,13 +1,20 @@
-  // JavaScript to make the div behave like sticky
-const stickyDiv = document.getElementById('sticky');
-const initialTop = stickyDiv.getBoundingClientRect().top + window.scrollY;
+const isMobile = window.matchMedia('(max-width: 768px)').matches; // Mobile screen check
 
-window.addEventListener('scroll', () => {
-    if (window.scrollY > initialTop) {
-    // Keep it at the top if scrolled past its initial position
-    stickyDiv.style.top = `${window.scrollY}px`;
-    } else {
-    // Reset to original absolute position when scrolled above
-    stickyDiv.style.top = `${initialTop}px`;
-    }
-});
+
+if (isMobile) {
+    // JavaScript to make the div behave like sticky
+    const stickyDiv = document.getElementById('sticky');
+    const initialTop = stickyDiv.getBoundingClientRect().top + window.scrollY;
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > initialTop) {
+        // Keep it at the top if scrolled past its initial position
+        stickyDiv.style.top = `${window.scrollY}px`;
+        } else {
+        // Reset to original absolute position when scrolled above
+        stickyDiv.style.top = `${initialTop}px`;
+        }
+    });
+} else {
+    return;
+}
